dc52085d13a23ea2a8d84fa13dff2590c28bd6ba
Joseph Perez <perez.joseph.k@gmail.com>
2022-03-28 09:05:32 -0500

    first new commit

---
+++ js/morning-warmup.js
@@ -0,0 +1,9 @@
+1. Return a string in reverse
+ex. reverseString(‘hello’) === ‘olleh’
+let reverseString = (str) => {
+    return //Something
+};
+2. Return true if palindrome and false if not
+    ex. isPalindrome(‘racecar’) === ‘true’, isPalindrome(‘hello’) == false
+let isPalindrome = (str) => {
+};
