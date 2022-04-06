860e3a056f205bf7fd9a85471026a4bc21e733be
JoePerez11 <perez.joseph.k@gmail.com>
2022-04-06 12:19:26 -0500

    adding files for new exercise

--- js/morning-warmup.js
+++ js/morning-warmup.js
@@ -101,3 +101,14 @@ library.forEach(function(obj) {
 var inputs = [1, 2, 3, 4, 5, 6]
 var result = inputs.filter(x => x % 2 == 0)
 console.log(result);
+
+// example input: [5,8,2,6,9,3,2];
+// expected output: [2,2,3,5,6,8,9] // length: 7
+// some possible test cases to think about:
+//     should be called ‘sortArr’
+// function should return an array
+// function should accept an array
+// BONUS thoughts
+// what would happen if you had negative numbers in your list
+// can you sort and then remove any duplicates (would it be more/less efficient to remove duplicates and then sort?)
+//if your array has non-numbers can you still sort the numbers? ex: input: [3,“b”,8,5,9,true”, 4,“xyz”] expected output: [3,4,5,8,9];
