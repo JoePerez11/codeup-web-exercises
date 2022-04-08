3cde8a79f47b781b92644a564e297ff4dafdf4a5
JoePerez11 <perez.joseph.k@gmail.com>
2022-04-08 08:39:23 -0500

    working on bonus

--- js/morning-warmup.js
+++ js/morning-warmup.js
@@ -112,3 +112,105 @@ console.log(result);
 // what would happen if you had negative numbers in your list
 // can you sort and then remove any duplicates (would it be more/less efficient to remove duplicates and then sort?)
 //if your array has non-numbers can you still sort the numbers? ex: input: [3,“b”,8,5,9,true”, 4,“xyz”] expected output: [3,4,5,8,9];
+
+// 1. Make a function named isOdd(number)
+
+function isOdd(number) {
+    if(number % 2 !== 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 2. Make a function named isEven(number)
+
+// 3. Make a function named isMultipleOfFive(input)
+
+// 4. Make a function named isMultipleOf(target, n) which checks if target is evenly divisible by n
+
+//5. Make a function named isVowel(letter)
+
+// ** the questions with an asterisk * are more challenging
+//
+// 6. Write a function called first(input) that returns the first character in the provided string.
+
+function first(input) {
+    var arr1 = input.split("");
+    return arr1[0];
+}
+//Made it better, faster, stronger in honor of Daft Punk
+function first(input) {
+    return input.split("")[0];
+}
+
+// 7* Create a function that takes in two string inputs.
+// -- If the second string input is present in the first, return the first input string with the second input string removed from it.
+// -- If the second string input is present multiple times in the first, the second string will only be removed where it first occurs in the first string.
+// -- If the second string input is not present in the first, return the first string as entered in the function.
+
+
+
+// 8. Make a function named isSpace(letter) that returns if a character is a space character
+
+
+
+// 9. Write a function named squareRoot(n) that returns the square root of the input
+
+
+
+// 10* create a function that will return how many whitespace characters are at the beginning of a string (hint: use regex).
+function whiteSpace(input) {
+    return '    input'.search(/\S/);
+}
+whiteSpace("purple");
+
+// 11. Write a function named invertSign(number) that returns a negative version of a postive number, a positve version of negative, and false for all else.
+
+
+
+// 12. Write a function named getRandomQuote().
+//   Inside of the function, create an array of strings where each string is a quote or thought you find inspirational
+//   getRandomQuote should generate a random number between 0 and the array's length minus 1
+//   use the randomly generated number as your index
+//   return a random quote.
+
+//13. Write a function named firstTenFibonacciNumbers() that returns an array of the first ten fibonacci numbers
+
+
+
+//1. Define a function named lowerCase that takes in an input and returns that input as a string in all lower case
+
+//2. Define a function named isAllLowerCase that takes in a string and returns true if all characters in the string are
+// lowercase otherwise it should return false.
+
+//3. Define a function named isAllUpperCase that takes in a string and returns true if all characters in the string are
+// uppercase otherwise it should return false.
+
+//4. Define a function named isNotPalindrome that takes in a string and returns true if that string is not palindrome,
+// if the string is palindrome should return false.
+
+//5. Define a function named multiplyBy2 that takes in an input and multiplies it by 2 if the input is numeric. If the
+// input is Not A Number, then return false.
+
+//6. Define a function named convertHourToSec that takes in one input Hours. Return the conversation of the number of
+// hours into total seconds. If the input is not numeric or a numeric string, convertHourToSec, should return false.
+
+//7. Write a function named getLowestNumber that takes in 3 arguments. If all 3 inputs are numbers or numeric strings,
+// then return the lowest number. If any of the 3 inputs is missing or non-numeric, then return false.
+
+//8. Write a function named addStringLengths that takes in two inputs. If both inputs provided are strings,
+// addStringLengths returns the sum after adding the length (number of characters) of both strings. If either or both
+// inputs are not strings, return false.
+
+//9. Write a function named subtract that takes in two inputs. If both inputs provided are numeric or numeric strings,
+// subtract will return the difference when the second input is subtracted from the first input.
+// If one or both inputs is not numeric or numeric strings, subtract should return false.
+
+//10. Write a function named calculateChange that takes in two inputs, totalPaid and totalCost.
+// If both inputs are numeric or numeric strings, calculateChange should return the change after subtracting the cost
+// from the amount paid. The return should be in $x.xx format as a string Note: it should return with two decial places.
+// If either or both inputs are not numeric or numeric strings, calculateChange should return false.
+
+
+
