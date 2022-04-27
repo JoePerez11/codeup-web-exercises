0e8c3c7116cd8961bf47e6ed14b4ddc4ebeff5b4
JoePerez11 <perez.joseph.k@gmail.com>
2022-04-27 17:16:50 -0500

    finished jquery selectors exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,5 +1,11 @@
 "use strict";
 
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
-});
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+
+$('.codeup').css('border', ' 1px solid red');
+$('li').css('font-size', '20px');
+$('h1, p, li').css('background-color', 'teal');
+var h1Element = $('h1').text();
+alert(h1Element);
